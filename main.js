0d181179208088deb7ccc2fb5586dc5b61413453
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-09 00:41:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function log(text) {
 }
 if (params.client_id && params.password && params.t) {
   log(params.t);
-  log(JSON.stringify(JSON.parse(atob(params.t.split('.')[1])), 2));
+  log(JSON.stringify(JSON.parse(atob(params.t.split('.')[1])), null, 2));
   log('loading...')
   
   var userId = JSON.parse(JSON.parse(atob(params.t.split('.')[1])).u).u.Id;
@@ -17,7 +17,7 @@ if (params.client_id && params.password && params.t) {
     headers: { Authorization: 'Basic '+btoa(params.client_id+':'+params.password) }
   })
     .then(response => response.json())
-    .then(result => log(JSON.stringify(result, 2)))
+    .then(result => log(JSON.stringify(result, null, 2)))
     .catch(err => log(String(err)));
 } else if (params.client_id && params.password && params['?t']) {
   window.open(location.origin+location.pathname+'#'+new URLSearchParams({client_id:params.client_id,password:params.password,t:params['?t']}),'_top');
