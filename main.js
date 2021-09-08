438ccbcccca22963219f7dfd27b352d670615cb1
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-08 20:09:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var params = location.search.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/)).reduce((o,[k,v])=>Object.assign(o,{[k]:v}),{});
 console.log(params);
 var hashParams =location.hash.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/)).reduce((o,[k,v])=>Object.assign(o,{[k]:v}),{});
-if (params.t && hashParams.t) window.open(location.origin+location.pathname+'#t='+params.t;
+if (params.t && hashParams.t) window.open(location.origin+location.pathname+'#t='+params.t,'_top');
