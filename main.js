98d569dc21c1d9c6094b2485de74639ab89eb5de
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-09 00:49:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ if (params.b && params.t) {
   var url = 'https://spaces.nexudus.com/api/sys/users/' + userId;
   fetch(url, {
     method: 'GET',
-    headers: { Authorization: 'Basic '+params.b }
+    headers: { Authorization: 'Bearer '+params.t.substr(1) }
   })
     .then(response => response.json())
     .then(result => log(JSON.stringify(result, null, 2)))
