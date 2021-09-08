7ed10de157faaedad710188bb2f20e801dfb0bb3
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-08 20:59:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ var hashParams =location.hash.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/).m
 var root = document.getElementById('root');
 if (hashParams.t) {
   root.innerText = 'loading...';
-  fetch("https://theexecutivecentre.spaces.nexudus.com/en/user/me", {
+  fetch("https://spaces.nexudus.com/api/auth/me", {
+    method: "GET",
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json",
       Authorization: "Bearer "+hashParams.t
     },
-    method: "GET",
+    body: null,
     mode: "cors"
   }).then(r => r.json()).then(result => {
     console.log({result});
@@ -18,7 +18,8 @@ if (hashParams.t) {
     pre.innerText = JSON.stringify(result, 2);
     root.innerHTML = '';
     root.appendChild(pre);
-  });
+  })
+  .catch(err => console.error(err));
 } else if (params.t) {
   window.open(location.origin+location.pathname+'#t='+encodeURIComponent(params.t),'_top');
 } else {
