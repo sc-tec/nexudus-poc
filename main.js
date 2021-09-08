8ad72c80f3d33e3cf75521306917f633230e08bd
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-08 23:10:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var params = location.search.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/).ma
 console.log(params);
 var hashParams =location.hash.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/).map(decodeURIComponent)).reduce((o,[k,v])=>Object.assign(o,{[k]:v}),{});
 var root = document.getElementById('root');
-if (hashParams.t) {
-  root.innerText = 'loading...\n' + hashParams.t;
+function log(text) {
   var pre = document.createElement('pre');
-  pre.innerText = JSON.stringify(JSON.parse(atob(hashParams.t.split('.')[1])), 2);
+  pre.innerText = text;
   root.appendChild(pre);
+}
+if (hashParams.t) {
+  log('loading...')
+  log(hashParams.t);
+  log(JSON.stringify(JSON.parse(atob(hashParams.t.split('.')[1])), 2));
   
   var userId = JSON.parse(JSON.parse(atob(hashParams.t.split('.')[1])).u).u.Id;
   var options = {
@@ -17,14 +21,8 @@ if (hashParams.t) {
   var url = 'https://thingproxy.freeboard.io/fetch/http://spaces.nexudus.com/api/sys/users/' + userId;
   fetch(url, options)
     .then(response => response.json())
-    .then(result => {
-      console.log({result});
-      var pre = document.createElement('pre');
-      pre.innerText = JSON.stringify(result, 2);
-      root.innerHTML = '';
-      root.appendChild(pre);
-    })
-    .catch(err => root.innerText = String(err));
+    .then(result => log(JSON.stringify(result,2)))
+    .catch(err => log(String(err)));
 } else if (params.t) {
   window.open(location.origin+location.pathname+'#t='+encodeURIComponent(params.t),'_top');
 } else {
