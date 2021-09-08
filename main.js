5cc9641a43121ad85afd1e859cc37c1b71989c59
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-08 20:20:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 var params = location.search.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/)).reduce((o,[k,v])=>Object.assign(o,{[k]:v}),{});
 console.log(params);
 var hashParams =location.hash.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/)).reduce((o,[k,v])=>Object.assign(o,{[k]:v}),{});
-if (params.t && !hashParams.t) window.open(location.origin+location.pathname+'#t='+params.t,'_top');
+var root = document.getElementById('root');
+if (hashParams.t) {
+  root.innerText = 'loading...';
+  fetch("https://spaces.nexudus.com/api/auth/me", {
+    "headers": {
+      "accept": "application/json",
+      "authorization": "Bearer "+hashParam.t,
+      "content-type": "application/json"
+    },
+    "method": "GET",
+    "mode": "cors",
+  }).then(result => {
+    console.log({result});
+    var pre = document.createElement('pre');
+    pre.innerText = JSON.stringify(result, 2);
+    root.innerHTML = '';
+    root.appendChild(pre);
+  });
+} else if (params.t) {
+  window.open(location.origin+location.pathname+'#t='+params.t,'_top');
+} else {
+  root.innerText = 'hello!';
+}
