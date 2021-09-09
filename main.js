b052c0d8346b5f729b2317532d2e685d87a54a9c
Steven Chong <62237840+steven-chong-tec@users.noreply.github.com>
2021-09-09 10:10:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-var params =location.hash.substr(1).split('&').map(l=>l.split(/(?<!=.*)=/).map(decodeURIComponent)).reduce((o,[k,v])=>Object.assign(o,{[k]:v}),{});
+var params =location.hash.substr(1).split('&')
+  .map(function(l) { return l.split(/(?<!=.*)=/).map(decodeURIComponent) })
+  .reduce(function(o, s) { return Object.assign(o, { [ s[0] ]: s[1] }) }, {})
 var root = document.getElementById('root');
-if (params.b && params.t) {
-  log(params.b)(params.t)(JSON.stringify(JSON.parse(atob(params.t.split('.')[1])), null, 2))('loading...')
-  
-  var userId = JSON.parse(JSON.parse(atob(params.t.split('.')[1])).u).u.Id;
-  var url = 'https://spaces.nexudus.com/api/sys/users/' + userId;
-  fetch(url, {
-    method: 'GET',
-    headers: { Authorization: 'Basic '+params.b }
-  })
-    .then(response => response.json())
-    .then(result => (log(JSON.stringify(result, null, 2)),log('AccessToken is '+(result.AccessToken==params.t?'valid.':'NOT valid.')),result))
-    .catch(err => log(String(err)));
-} else if (params.b && params['?t']) {
-  window.open(location.origin+location.pathname+'#'+new URLSearchParams({b:params.b,t:params['?t'].replace(/^\$/,'')}),'_top');
-} else {
-  log(location.href);
+try {
+  if (params.b && params['?t']) {
+    window.open(location.origin+location.pathname+'#'+new URLSearchParams(Object.keys(params).reduce(function(o, k) {
+      return Object.assign(o, k === '?t' ? 
+        { t: params[k].replace(/^\$/, '') } :
+        { [ k ]: params[k] }
+      ), {}
+    })), '_top')
+  } else if (params.b && params.t) {
+    log(params.b, params.t, JSON.stringify(JSON.parse(atob(params.t.split('.')[1])), null, 2), 'loading...')
+    var userId = JSON.parse(JSON.parse(atob(params.t.split('.')[1])).u).u.Id
+    var url = 'https://spaces.nexudus.com/api/sys/users/' + userId
+    fetch(url, {
+      method: 'GET',
+      headers: { Authorization: 'Basic ' + params.b }
+    })
+      .then(function(response) { return response.json() })
+      .then(function(result) { log(
+        JSON.stringify(result, null, 2),
+        'AccessToken ' + (result.AccessToken === params.t ? 'is' : 'is NOT') + ' valid.'
+      ) })
+      .catch(function(err) { log(String(err)) })
+  } else {
+    log(location.href)
+  }
+} catch (ex) {
+  log(String(ex))
 }
-function log(text) {
-  return (root.appendChild(Object.assign(document.createElement('pre'),{innerText:text})),log);
+function log() {
+  for (var i = 0; i < arguments.length; i++)
+    root.appendChild(Object.assign(document.createElement('pre'), { innerText: text }))
 }
